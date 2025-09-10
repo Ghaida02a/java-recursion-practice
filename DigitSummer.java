a92c7028afa04a301e179a2aabb139d319e7e791
Ghaida <ggmskaj@gmail.com>
2025-09-10 08:45:49 +0400

    Task 8: Sum of Digits of a Number added

---
+++ DigitSummer.java
@@ -0,0 +1,25 @@
+public class DigitSummer {
+
+    public static void main(String[] args) {
+        int number = 12345;
+        int tempNumber = number;
+
+        System.out.println("Calculating the sum of digits for " + number + " with a loop...");
+        int sum = sumDigitsRecursive(number);
+//        // --- START OF BLOCK TO REPLACE ---
+//        while (tempNumber > 0) {
+//            int lastDigit = tempNumber % 10; // Get the last digit
+//            sum += lastDigit;
+//            tempNumber = tempNumber / 10; // Remove the last digit
+//        }
+//        // --- END OF BLOCK TO REPLACE ---
+
+        System.out.println("The sum of the digits is: " + sum);
+    }
+    public static int sumDigitsRecursive(int number){
+        if(number == 0){
+            return 0;
+        }
+        return (number % 10) + sumDigitsRecursive(number / 10);
+    }
+}
