082abe18a0e8da90f3ea4a16713a4992844e362d
Ghaida <ggmskaj@gmail.com>
2025-09-14 14:33:32 +0400

    Task 14: Summing a Nested List of Objects removed unnecessary comments

--- Item.java
+++ Item.java
@@ -55,27 +55,4 @@ class ValueCalculator {
         }
         return sum;
     }
-//    public static void main(String[] args) {
-//        // Create individual items
-//        Container c1 = new Container(new Item(10));
-//        Container c2 = new Container(new Item(20));
-//        Container c3 = new Container(new Item(30));
-//
-//        // Nest some containers
-//        List<Container> nested1 = new ArrayList<>();
-//        nested1.add(c1);
-//        nested1.add(c2);
-//        Container group1 = new Container(nested1);
-//
-//        List<Container> nested2 = new ArrayList<>();
-//        nested2.add(group1);
-//        nested2.add(c3);
-//        Container topLevel = new Container(nested2);
-//
-//        // Calculate total value
-//        ValueCalculator calculator = new ValueCalculator();
-//        int total = calculator.calculateTotalValue(topLevel);
-//
-//        System.out.println("Total value of all items: " + total);
-//    }
 }
